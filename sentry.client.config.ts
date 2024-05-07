c91961267495e9b71ee646eebd5de4843d8095e0
Caleb John <johncaleb022@gmail.com>
2024-05-07 08:35:02 -0700

    Update sentry.client.config.ts

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,4 +1,3 @@
-
 // This file configures the initialization of Sentry on the client.
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
